293e9e301e894b22e01871f5c3044384c72e2b7c
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-08-25 17:42:16 +0630

    Finished setting up router

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw,
+} from "vue-router";
+import Home from "@/views/home.vue";
+import Vuex from "@/views/vuex.vue";
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/vuex",
+    name: "Vuex",
+    component: Vuex,
+  },
+];
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+export default router;
