a4dfcab577a446e6d4b68bfb50d24f9364df4cb4
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-08-26 13:45:02 +0630

    test(tests/test.spec.ts, package.js, jest.config.js, tsconfig.json): setting up TDD

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     '@vue/typescript/recommended',
     'plugin:vue/vue3-strongly-recommended',
-    'plugin:vue/vue3-recommended'
+    'plugin:vue/vue3-recommended',
+    'plugin:jest/recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
